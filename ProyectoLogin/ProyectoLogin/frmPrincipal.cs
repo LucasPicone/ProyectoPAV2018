f792a424bf9c3419f06c01dfe2999e5b6317210f
LucasPicone <42209166+LucasPicone@users.noreply.github.com>
2018-08-09 13:10:29 -0300

    Add files via upload

---
+++ ProyectoLogin/ProyectoLogin/frmPrincipal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoLogin
+{
+    public partial class frmPrincipal : Form
+    {
+        private string user = "admin";
+        private string pass = "admin";
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            frmLogin fl;
+            fl = new frmLogin();
+            fl.ShowDialog();
+        }
+        public bool validarUsuario(string u, string p)
+        {
+            if (this.user == u && this.pass == p)
+                return true;
+            else
+                return false;
+        }
+    }
+}
