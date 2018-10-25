86c27d32950dd37ecf07653622b1bd145d8748a7
LucasPicone <42209166+LucasPicone@users.noreply.github.com>
2018-10-25 12:50:10 -0300

    reportes

---
+++ transaccion reportes/transaccion/reporte2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace transaccion
+{
+    public partial class reporte2 : Form
+    {
+        BDHelper oDatos = new BDHelper();
+        public reporte2()
+        {
+            InitializeComponent();
+        }
+
+        private void reporte2_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'BUGSDataSet.Bugs' Puede moverla o quitarla según sea necesario.
+            //this.BugsTableAdapter.Fill(this.BUGSDataSet.Bugs);
+
+            //this.reportViewer1.RefreshReport();
+        }
+
+        private void cmdBotoncito_Click(object sender, EventArgs e)
+        {
+            SqlCommand comando = new SqlCommand("select * from bugs");
+            this.BugsBindingSource.DataSource = oDatos.ConsultaSQL(comando);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
