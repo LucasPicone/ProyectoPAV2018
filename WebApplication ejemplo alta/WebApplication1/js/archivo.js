ded56a18740777b6d156990f2b9cddc6716c8cb1
LucasPicone <42209166+LucasPicone@users.noreply.github.com>
2019-05-02 17:04:13 -0300

    Add files via upload

---
+++ WebApplication ejemplo alta/WebApplication1/js/archivo.js
@@ -0,0 +1,31 @@
+﻿var miApp = angular.module("miApp", []);
+
+miApp.controller("myCtrl", function ($scope, $http) {
+    //$scope.paises = [{ name: "argentina", region: "sudamerica", population: "54000000" }, { name: "brasil", region: "sudamerica"}, { name: "urgay", region: "sudamerica", population: "5000000 canguros" }];
+    $scope.mostrar = true;
+    $scope.cargarPaises = function () {
+        $scope.mostrar = false;
+        $http({ method: 'GET', url: 'https://restcountries.eu/rest/v2/all' }).then(function successCallback(response) {
+            $scope.mostrar = true;
+            $scope.paises = response.data;
+        }, function errorCallback(response) {
+            $scope.mostrar = true;
+            window.alert("rompiste todo flaque");
+        });
+    }
+    $scope.poblacion = function (pais) {
+        window.alert("La Poblacion es: " + pais.population)
+    };
+    $scope.guardar = function () {
+        if ($scope.frmPais.$valid) {
+            $scope.paises.push({ name: $scope.paisName, region: $scope.paisRegion });
+            $scope.paisName = '';
+            $scope.paisRegion = '';
+            //window.alert('Formulario Valido');
+        }
+        else
+        {
+            window.alert('Formulario Invalido')
+        }
+    };
+});
